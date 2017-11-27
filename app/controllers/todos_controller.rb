b2ac21808834f42c549ab8c8def1a6e65d2f6561
Dan Actor <danltactor@gmail.com>
2017-11-27 16:22:40 -0700

    complete basic objectives

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,12 +12,31 @@ class TodosController < ApplicationController
   end
 
   def create
-    @todo = Todo.new(todo_params)
+    @todo = Todo.new(todos_params)
     if @todo.save
       redirect_to todos_path
     else
       render :new
     end
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(todos_params)
+      redirect_to todos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Todo.find(params[:id]).destroy
+    redirect_to todos_path
   end
 
   private
